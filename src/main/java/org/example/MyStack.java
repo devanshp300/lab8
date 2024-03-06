aec6fe0cb9bea046d22b7776a1ffb76a5f159fe9
Devansh291001 <devanshpatel2910@icloud.com>
2024-03-06 15:07:14 -0500

    Fixed push() method to correctly add elements to the stack

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyStack extends ArrayList {
+    private ArrayList<Object> delegate;
+
+    public MyStack() {
+        this.delegate = new ArrayList<>();
+    }
+
+    public void push(Object item) {
+        this.add(item);
+    }
 
 
 
