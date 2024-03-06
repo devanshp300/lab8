8f81790315d6c9bd347476c49904290d06d5e08b
Devansh291001 <devanshpatel2910@icloud.com>
2024-03-06 15:06:13 -0500

    failing test for push

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -3,17 +3,8 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-public class MyStack<E> extends ArrayList<E> {
+public class MyStack extends ArrayList {
 
-    public void push(E e) {
-        add(e);
-    }
 
-    public E pop() {
-        if (isEmpty()) throw new EmptyStackException();
-        E e = get(size() - 1);
-        remove(size() -1 );
-        return e;
-    }
 
 }
---
+++ src/test/java/org/example/MyStackTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MyStackTest
+{
+    @Test
+    void testPushAndPop() {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(2);
+
+    }
+
+
+
+}
